213a6b6a5e3493a024a3499c667ac855dbc24cc1
kkien1903 <luuthikimyen2865@gmail.com>
2026-04-22 18:04:06 +0700

    Update initialization spring boot

---
+++ src/main/java/com/projetc/menugame/MenugameApplication.java
@@ -0,0 +1,13 @@
+package com.projetc.menugame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenugameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenugameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetc/menugame/MenugameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetc.menugame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenugameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
